a9bada54a27af872897f9b64632881a58c4ee442
S.Watanabe <ocelot2828@gmail.com>
2024-07-26 15:50:25 +0900

    feat: add animation2 and animation3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Installation from "./pages/Installation";
 import Animation1 from "./pages/Animation1";
+import Animation2 from "./pages/Animation2";
+import Animation3 from "./pages/Animation3";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Installation />} />
       <Route path="/animation1" element={<Animation1 />} />
+      <Route path="/animation2" element={<Animation2 />} />
+      <Route path="/animation3" element={<Animation3 />} />
     </Routes>
   );
 };
